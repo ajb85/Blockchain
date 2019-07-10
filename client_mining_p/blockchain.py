9b6335a3dcff48d90fe5bca1c13134a13611edd1
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-07-10 18:07:23 -0400

    Completes blockchain day 1

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -146,21 +146,20 @@ def mine():
     # We run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
     last_proof = last_block['proof']
+    proof = req["proof"]
     #proof = blockchain.proof_of_work(last_proof)
 
     # We must receive a reward for finding the proof.
     # The sender is "0" to signify that this node has mine a new coin
-    if(blockchain.valid_proof(last_proof, req["proof"])):
+    if(blockchain.valid_proof(last_proof, proof)):
         blockchain.new_transaction(
             sender="0",
             recipient=req["recipient"] if "recipient" in req else "0",
             amount=1,
         )
-
         # Forge the new Block by adding it to the chain
         previous_hash = blockchain.hash(last_block)
         block = blockchain.new_block(proof, previous_hash)
-
         response = {
             'message': "New Block Forged",
             'index': block['index'],
@@ -168,12 +167,13 @@ def mine():
             'proof': block['proof'],
             'previous_hash': block['previous_hash'],
         }
+        return jsonify(response), 200
     else:
         response = {
-            "message": "Invalid proof"
+            "message": False
         }
+        return jsonify(response), 401
 
-    return jsonify(response), 200
 
 @app.route('/last_proof', methods=['GET'])
 def last_proof():
